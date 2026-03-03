a261ea7e934d8c61dbca6e27760a4100ad6167fa
mohamedB45 <bouzidi.mohamed201@gmail.com>
2026-03-03 22:21:04 +0100

    Rename class from ex03 to ex02

---
+++ TP/ex02.java
@@ -0,0 +1,12 @@
+public class ex02 {
+public static void main(String[] args){
+   int i,j;
+  
+for(i=0;i<4;i++){
+    for(j=0;j<=i;j++){
+     System.out.print("*");
+    }
+    System.out.print("\n");
+}
+}
+}
