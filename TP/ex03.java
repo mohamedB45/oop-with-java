3dcbc940a7dd1285bd7d6bfffdc49d0ea1715bd4
mohamedB45 <bouzidi.mohamed201@gmail.com>
2026-03-03 22:24:32 +0100

    Create ex03.java

---
+++ TP/ex03.java
@@ -0,0 +1,12 @@
+public class ex03 {
+public static void main(String[] args){
+   int i;
+int j;
+for(i=0;i<4;i++){
+    for(j=0;j<4-i;j++){
+     System.out.print("*");
+    }
+    System.out.print("\n");
+}
+}   
+}
